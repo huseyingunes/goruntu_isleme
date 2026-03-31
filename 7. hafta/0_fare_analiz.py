0ccab1c497714e06382c40e970166820837f8618
Hüseyin GÜNEŞ <huseyingunes@gmail.com>
2026-03-31 14:23:10 +0300

    ders içinde güncellemeler

--- 7. hafta/0_fare_analiz.py
+++ 7. hafta/0_fare_analiz.py
@@ -9,8 +9,16 @@ while video.isOpened():
     if i < 270:
         continue
     grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    grayFrame = grayFrame[90:900, 400:1350]
-    #grayFrame = cv.medianBlur(grayFrame, 5)
+    grayFrame = grayFrame[75:910, 430:1320]
+
+
+
+    pts1 = np.float32([[74, 5], [860, 53], [40, 786], [806, 825]])
+    pts2 = np.float32([[0, 0], [500, 0], [0, 500], [500, 500]])
+    M = cv.getPerspectiveTransform(pts1, pts2)
+    grayFrame = cv.warpPerspective(grayFrame, M, (500, 500))
+
+    grayFrame = cv.medianBlur(grayFrame, 5)
     ret, esiklenmis = cv.threshold(grayFrame, 180, 255,
                                    cv.THRESH_BINARY)
     if ret:
